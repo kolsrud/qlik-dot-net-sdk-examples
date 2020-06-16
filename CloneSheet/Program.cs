5133576529cf7c42d6031bb0a6df7dd1baf71730
Øystein Kolsrud <yko@qlik.com>
2020-06-16 11:43:18 +0200

    Ensured correct cell updates.

--- CloneSheet/Program.cs
+++ CloneSheet/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Qlik.Engine;
 using Qlik.Sense.Client;
 
@@ -15,15 +16,18 @@ namespace CloneSheet
 			var location = Location.FromUri(url);
 			location.AsNtlmUserViaProxy();
 			var appId = location.AppWithNameOrDefault(appName);
-			using (var app = location.App(appId)) 
+			using (var app = location.App(appId))
 			{
 				var destinationSheetId = app.CloneGenericObject(sourceSheetId);
+				var sourceSheet = app.GetGenericObject(sourceSheetId);
 				var destinationSheet = app.GetGenericObject(destinationSheetId);
 				var destinationSheetProps = destinationSheet.Properties.As<SheetProperties>();
-				var childInfos = destinationSheet.GetChildInfos();
-				foreach (var (destinationCell, childInfo) in destinationSheetProps.Cells.Zip(childInfos))
+				var sourceChildInfos = sourceSheet.GetChildInfos();
+				var destinationChildInfos = destinationSheet.GetChildInfos();
+				var childIdMap = new Dictionary<string, string>(sourceChildInfos.Zip(destinationChildInfos, (i0, i1) => new KeyValuePair<string,string>(i0.Id, i1.Id)));
+				foreach (var destinationCell in destinationSheetProps.Cells)
 				{
-					destinationCell.Name = childInfo.Id;
+					destinationCell.Name = childIdMap[destinationCell.Name];
 				}
 
 				destinationSheetProps.MetaDef.Title = "Clone";
