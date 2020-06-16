85ada8b83df6220c8d2f86208966be9fc8e37c25
Øystein Kolsrud <yko@qlik.com>
2020-06-16 11:29:58 +0200

    Added example illustrating cloning of sheet.

---
+++ CloneSheet/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using Qlik.Engine;
+using Qlik.Sense.Client;
+
+namespace CloneSheet
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var url = "https://<url>";
+			var appName = "<appName>";
+			var sourceSheetId = "<sheetId>";
+
+			var location = Location.FromUri(url);
+			location.AsNtlmUserViaProxy();
+			var appId = location.AppWithNameOrDefault(appName);
+			using (var app = location.App(appId)) 
+			{
+				var destinationSheetId = app.CloneGenericObject(sourceSheetId);
+				var destinationSheet = app.GetGenericObject(destinationSheetId);
+				var destinationSheetProps = destinationSheet.Properties.As<SheetProperties>();
+				var childInfos = destinationSheet.GetChildInfos();
+				foreach (var (destinationCell, childInfo) in destinationSheetProps.Cells.Zip(childInfos))
+				{
+					destinationCell.Name = childInfo.Id;
+				}
+
+				destinationSheetProps.MetaDef.Title = "Clone";
+				destinationSheet.SetProperties(destinationSheetProps);
+				app.DoSave();
+			}
+		}
+	}
+}
